3c9f30db61cbcde0e702f4d1d175adee5329b2a5
MarcosBremermann <mabm2004@gmail.com>
2023-09-08 20:03:18 +0000

    Task 0 v1.0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+#include <stddef.h>
+/**
+ * function that sorts array of integers using the bubble sort algorithm
+ * @array: the array to be sorted
+ * @size: size of the array
+*/
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+    int swapped;
+
+    if (size < 2)
+    {
+        return;
+    }
+
+    for (i = 0; i < size - 1; i++)
+    {
+        swapped = 0;
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                int temp;
+
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                print_array(array, size);
+                swapped = 1;
+            }
+        }
+        if (swapped == 0)
+        {
+            break;
+        }
+    }
+}
---
+++ sort.h
@@ -0,0 +1,15 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+typedef struct listint_s {
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+
+#endif
