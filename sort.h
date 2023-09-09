3e734de813452f147aaaae52b8c5d2a2d69cc778
MarcosBremermann <mabm2004@gmail.com>
2023-09-09 01:38:29 +0000

    Task 0 v1.02: Betty TAB fixes

--- sort.h
+++ sort.h
@@ -2,10 +2,17 @@
 #define SORT_H
 
 #include <stddef.h>
-
-typedef struct listint_s {
-    int n;
-    struct listint_s *next;
+/*
+* struct listint_s - Singly linked list node
+* @n: integer stored in the node
+* @next: pointer to the next node in the list
+*
+* Description: This structure defines a singly linked list node
+*/
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 void print_list(const listint_t *list);
