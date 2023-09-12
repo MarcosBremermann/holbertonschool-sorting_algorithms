a263bc6cfeaa3b0762c66d937e7cbb12239b1517
MarcosBremermann <mabm2004@gmail.com>
2023-09-12 15:47:30 +0000

    Task 1 v1.0

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - function that sorts via insertion sort
+ * @list: list to be sorted
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *sorted, *next;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
+
+	current = *list;
+	while (current)
+	{
+		sorted = current->next;
+		next = current->next;
+
+		while (sorted && sorted->prev && sorted->n < sorted->prev->n)
+		{
+			sorted->next ? (sorted->next->prev = sorted->prev) : 0;
+			sorted->prev->next = sorted->next;
+			sorted->next = sorted->prev;
+			sorted->prev = sorted->prev->next;
+			sorted->next->prev = sorted;
+			sorted->prev ? (sorted->prev->next = sorted) : (*list = sorted);
+			sorted->next->next ? (sorted->next->next->prev = sorted->next) : 0;
+			sorted->next->next = sorted;
+			sorted->prev ? (current = sorted->prev) : (current = sorted);
+			print_list(*list);
+		}
+
+		current = next;
+	}
+}
