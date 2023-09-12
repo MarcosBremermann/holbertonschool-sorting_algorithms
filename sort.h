17e4f1f4732b1382159aa271c511400c47ab31c6
MarcosBremermann <mabm2004@gmail.com>
2023-09-12 15:19:15 +0000

    header updated

--- sort.h
+++ sort.h
@@ -3,9 +3,10 @@
 
 #include <stddef.h>
 
-/*
+/**
 * struct listint_s - Singly linked list node
 * @n: integer stored in the node
+* @prev: pointer to the previous node in the list
 * @next: pointer to the next node in the list
 *
 * Description: This structure defines a singly linked list node
@@ -13,11 +14,13 @@
 typedef struct listint_s
 {
 	int n;
+	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif
