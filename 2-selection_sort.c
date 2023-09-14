08642b40ac3d8473b72c0134f266f1f16b49beaf
MarcosBremermann <mabm2004@gmail.com>
2023-09-14 20:10:50 +0000

    Task 2 v1.02: selection_sort errors

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,25 +7,18 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
-	int temp;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min_idx = i + 1;
-
+		min_idx = i;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
-			{
 				min_idx = j;
-			}
 		}
-
-		if (min_idx != i + 1)
+		if (min_idx != i)
 		{
-			temp = array[i + 1];
-			array[i] = array[min_idx];
-			array[min_idx] = temp;
+			swap(&array[min_idx], &array[i]);
 			print_array(array, size);
 		}
 	}
