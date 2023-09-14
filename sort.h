b6173a52b99df63e0521faa6750e588e7a9b35c5
MarcosBremermann <mabm2004@gmail.com>
2023-09-14 19:00:36 +0000

    Task 2 v1.0: sort.h and 2-O files

--- sort.h
+++ sort.h
@@ -8,8 +8,6 @@
 * @n: integer stored in the node
 * @prev: pointer to the previous node in the list
 * @next: pointer to the next node in the list
-*
-* Description: This structure defines a singly linked list node
 */
 typedef struct listint_s
 {
@@ -18,9 +16,11 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* Prototypes */
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 #endif
