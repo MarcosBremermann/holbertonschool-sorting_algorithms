3a44f0c7009ec5fed6448acbb64d7fffb0966346
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-01-11 15:37:57 -0300

    task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,62 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
+ *                       using the Insertion sort algorithm
+ * @list: Pointer to the head of the doubly linked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			temp->prev->next = temp->next;
+
+			if (temp->next != NULL)
+
+				temp->next->prev = temp->prev;
+				temp->next = temp->prev;
+				temp->prev = temp->prev->prev;
+
+			if (temp->prev != NULL)
+				temp->prev->next = temp;
+
+			else
+				*list = temp;
+			temp->next->prev = temp;
+			print_list(*list);
+		}
+
+		current = current->next;
+	}
+}
+
+/**
+ * print_list - Prints a list of integers
+ *
+ * @list: The list to be printed
+ */
+void print_list(const listint_t *list)
+{
+	int i;
+
+	i = 0;
+	while (list)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", list->n);
+		++i;
+		list = list->next;
+	}
+	printf("\n");
+}
+
