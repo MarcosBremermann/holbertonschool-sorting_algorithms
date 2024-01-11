f50fe693307f49dcd19acb1c36b1094edc337198
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-01-11 16:38:08 -0300

    fix errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-#include "print_array.c"
 
 /**
  * bubble_sort - Sorts an array of integers in ascending order using Bubble Sort
@@ -9,22 +8,22 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i;
-    size_t j;
-    int temp;
+	size_t i;
+	size_t j;
+	int temp;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size - 1 - i; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                /* Swap values */
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				/* Swap values */
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
