d5213195420ec702af0dc46badec862e0732568c
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-01-11 16:17:19 -0300

    prototypes and libraries

--- sort.h
+++ sort.h
@@ -22,8 +22,13 @@ typedef struct listint_s
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void swap_nodes(listint_t *current, listint_t **list)
 
 
 #endif
