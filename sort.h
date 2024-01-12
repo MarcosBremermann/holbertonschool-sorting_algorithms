46e97e767333b60d660dd40f1e7d106460ab133c
MarcosBremermann <mabm2004@gmail.com>
2024-01-12 14:00:58 -0300

    Task 2 v1.0

--- sort.h
+++ sort.h
@@ -28,4 +28,7 @@ void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void swap_nodes(listint_t *current, listint_t **list);
+void selection_sort(int *array, size_t size);
+
+
 #endif
