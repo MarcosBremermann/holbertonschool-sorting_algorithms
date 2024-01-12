5a9d392e000bd9c3a7d913ecc199d4a40c32186c
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-01-12 17:46:25 -0300

    betty error

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,36 +1,36 @@
 #include "sort.h"
 
 /**
- * selection_sort - function that sorts integers using the Selection sort algorithm
+ * selection_sort - sorts integers using the Selection sort algorithm
  * @array: The array to be sorted
  * @size: Number of elements in @array
 */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_index;
-    int temp;
+	size_t i, j, min_index;
+	int temp;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; ++i)
-    {
-        min_index = i;
+	for (i = 0; i < size - 1; ++i)
+	{
+		min_index = i;
 
-        for (j = i + 1; j < size; ++j)
-        {
-            if (array[j] < array[min_index])
-                min_index = j;
-        }
+		for (j = i + 1; j < size; ++j)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
 
-        if (min_index != i)
-        {
-            /* Swap elementos */
-            temp = array[i];
-            array[i] = array[min_index];
-            array[min_index] = temp;
+		if (min_index != i)
+		{
+			/* Swap elementos */
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
 
-            print_array(array, size);
-        }
-    }
+			print_array(array, size);
+		}
+	}
 }
